bfc7c899d54f574fed5050b667d6316d621e6be6
Julien Etien <jetien@ubidreams.com>
2014-05-14 17:47:39 +0200

    fix bug on loading bundle classes : remove whitespace

--- wisdom-ebean/src/main/java/org/wisdom/ebean/runtime/EbeanExtender.java
+++ wisdom-ebean/src/main/java/org/wisdom/ebean/runtime/EbeanExtender.java
@@ -177,7 +177,7 @@ public class EbeanExtender implements BundleTrackerCustomizer<EbeanRepository> {
         List<Class<?>> list = new ArrayList<>();
         for (String s : classes) {
             try {
-                Class c = bundle.loadClass(s);
+                Class c = bundle.loadClass(s.trim());
                 list.add(c);
             } catch (ClassNotFoundException e) {
                 throw new ClassNotFoundException("Cannot load entity class " + s + " from bundle " + bundle
